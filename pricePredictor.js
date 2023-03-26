741055a8ac25af0058787cb675aefae4b8636a1a
alexC223 <08_basalts.tonnage@icloud.com>
2023-03-25 23:54:22 -0400

    Finished algo

--- pricePredictor.js
+++ pricePredictor.js
@@ -1,9 +1,4 @@
 
-//var script = document.createElement('script.');
-//script.src = 'script.js';
-// i dont think these two lines on top are needed
-
-
 // algo
 // I will use price from 60 minutes ago and 1 minute ago.
 // if stock price is above moving average, the algo thinks it will keep moving up
@@ -16,128 +11,29 @@ function pricePredictor(price, time, counter, lastMinutePrice, updateFrequency, 
     var start = time - 14210000;
     var end = time - 10000;
     return fetch(`https://api.coincap.io/v2/assets/bitcoin/history?interval=h2&start=${start}&end=${end}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log("here is the data from the time api");
-        console.log(data)
-        var latestPrice = parseInt(data.data[0].priceUsd);
-        var lastHourPrice = parseInt(data.data[data.data.length - 1].priceUsd);
-        if (counter >= lineDifference){ // was 10
-            var movingAverage = (latestPrice + lastHourPrice) / 2;
-            var pricePredicted = (movingAverage + lastMinutePrice) / 2;
-            
-            //console.log("It got to counter >=10 in pricepredictor.js");
-        }
-        if (counter < lineDifference){
-            var movingAverage = ((latestPrice + lastHourPrice) / 2);
-            var pricePredicted = movingAverage;
-        }
-        console.log("This is the last minute price according to pricePredictor.js " + lastMinutePrice);
-        console.log("This is the moving average according to pricePredictor.js " + movingAverage);
-        
-        
-  
-        console.log("This is the price predicted in pricePredictor.js " + pricePredicted);
-        return pricePredicted;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-
-
-
-
-/*
-function pricePredictor(price, time) {
-    return fetch('https://api.coincap.io/v2/assets/bitcoin/history?interval=h1')
         .then(response => response.json())
         .then(data => {
-            //console.log(data);
-            var lastMinutePrice = parseInt(data.data[10].priceUsd);
-            //console.log(lastMinutePrice);
-            var lastHourPrice = parseInt(data.data[700].priceUsd);
-            //console.log(lastHourPrice);
-            
-            var pricePredicted;
-            
-           
-            var movingAverage = (lastMinutePrice + lastHourPrice) / 2;
-            pricePredicted = movingAverage
-            
-            console.log("this is the moving average " + movingAverage);
-            
+            var latestPrice = parseInt(data.data[0].priceUsd);
+            var lastHourPrice = parseInt(data.data[data.data.length - 1].priceUsd);
+            if (counter >= lineDifference) {
+                var movingAverage = (latestPrice + lastHourPrice) / 2;
+                var pricePredicted = (movingAverage + lastMinutePrice) / 2;
 
+            }
+            if (counter < lineDifference) {
+                var movingAverage = ((latestPrice + lastHourPrice) / 2);
+                var pricePredicted = movingAverage;
+            }
+            pricePredicted = pricePredicted + Math.floor(Math.random()*5);
 
-            //var pricePredicted = 28000;
+            console.log("here is the data from the time api");
+            console.log(data)
+            console.log("This is the last minute price according to pricePredictor.js " + lastMinutePrice);
+            console.log("This is the moving average according to pricePredictor.js " + movingAverage);
             console.log("This is the price predicted in pricePredictor.js " + pricePredicted);
             return pricePredicted;
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
 }
-*/
-
-
-
-/*
-function pricePredictor(price, time){
-    
-    fetch('https://api.coincap.io/v2/assets/bitcoin/history?interval=h1')
-        .then(response => response.json())
-        .then(data => {
-            
-            console.log(data);
-            var lastMinutePrice = data.data[10].priceUsd;
-            console.log(lastMinutePrice);
-            var lastHourPrice = data.data[700].priceUsd;
-            console.log(lastHourPrice);
-        
-
-            var pricePredicted = 28000;
-            console.log(pricePredicted)
-
-            return pricePredicted;
-    
-    });
-    }
-    */
-
-        /*
-        var movingAverage = (lastMinutePrice + lastHourPrice) / 2;
-        
-        var pricePredicted = 0;
-        if (price >= movingAverage){
-            pricePredicted = price+5;
-        }
-        if (price < movingAverage){
-            pricePredicted = price-5;
-        }
-        */
-        
-        
-    
-
-
-    
-
-
-
-/*
-function updateChart() {
-            
-    fetch('https://api.coincap.io/v2/assets/bitcoin')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        var time = data.timestamp;
-        console.log(time);
-        var price = parseInt(data.data.priceUsd);
-        console.log(price);
-        
-        chart.data.labels.push(time);
-        chart.data.datasets[0].data.push(price);
-        chart.data.datasets[1].data.push(pricePredictor(price));
-
-        chart.update();
-    });
-}
-*/
--- script.js
+++ script.js
@@ -1,122 +1,98 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
-//var script = document.createElement('pricePredicted');
-//script.src = 'pricePredictor.js';
-// i dont think i need these either
-
 var time;
 var price;
 var counter = 0;
 var lastMinutePrice;
-var updateFrequency = 1000; // in milleseconds // worked with 7
-var lineDifference = 15; // how much ahead you want the predicted price to be // worked with 7
+var updateFrequency = 1000; // in milleseconds 
+var lineDifference = 15; // how much ahead you want the predicted price to be, multiplied by update freq
+var delay = updateFrequency*lineDifference;
 
-
-
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [{
-                label: 'Bitcoin Price',
-                data: [],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1,
-                
-            },
-            {
-                label: 'Predicted Bitcoin Price',
-                data: [],
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1,
-                
-        }]
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Bitcoin Price',
+            data: [],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
         },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false
-                },
-            }
+        {
+            label: 'Predicted Bitcoin Price',
+            data: [],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            },
         }
-    });
+    }
+});
 
-    
-    function updateChart() {
-        console.log("");
-        console.log("");
-        console.log("");
-            
-        fetch('https://api.coincap.io/v2/assets/bitcoin')
+
+function updateChart() {
+    console.log("");
+    console.log("");
+    console.log("");
+
+    fetch('https://api.coincap.io/v2/assets/bitcoin')
         .then(response => response.json())
         .then(data => {
             console.log("this is is normal data from assets ");
             console.log(data);
             var time = parseInt(data.timestamp);
+            var modifiedTime = (time + delay).toString;
+            //var modifiedTime = data.timestamp;
+            console.log("this is the modified time" + modifiedTime);
             console.log("this is the timestamp from assets " + time);
             var price = parseInt(data.data.priceUsd);
             console.log("this is the price from assets " + price);
+
+            modifiedTime = new Date(time + delay);
+            const time1 = modifiedTime.toISOString();
             
-            chart.data.labels.push(time);
-        
-            // the x and y format isnt working
-    
-            if (counter >= lineDifference){ // was 7
+
+            //console.log(`Time: ${time1}`);
+            //modifiedTime = new Date(modifiedTime);
+            chart.data.labels.push(time1);
+            
+            
+
+            if (counter >= lineDifference) { 
                 chart.data.datasets[0].data.push(price);
             }
-            
-            //chart.data.datasets[1].data.push(pricePredictor(price, time));
-            
-            if (counter >= lineDifference) { // was 7
+
+            if (counter >= lineDifference) {
                 if (data.data[data.data.length - 1] > 59) {
-                  var lastMinutePrice = chart.data.datasets[0].data[59];
+                    var lastMinutePrice = chart.data.datasets[0].data[59];
                 } else {
-                  var lastMinutePrice = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
+                    var lastMinutePrice = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
                 }
                 console.log("This is the last minute price according to script.js " + lastMinutePrice);
-              }
-            
-            
-            
+            }
 
             lastMinutePrice = parseInt(lastMinutePrice)
 
             pricePredictor(price, time, counter, lastMinutePrice, updateFrequency, lineDifference)
-            .then(price => {
-                console.log("This is the price predicted in script.js " + price);
-                chart.data.datasets[1].data.push(price);
-            })
-            .catch(error => {
-                console.error('Error:', error);
+                .then(price => {
+                    console.log("This is the price predicted in script.js " + price);
+                    chart.data.datasets[1].data.push(price);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
                 });
-
-
-                
 
             chart.update();
             ++counter;
         });
-    }
-        
-    
+}
 
-    setInterval(updateChart, updateFrequency);
+setInterval(updateChart, updateFrequency);
 
-        /*
-        fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => response.json())
-            .then(data => {
-            var time = new Date(data.time.updatedISO);
-            var price = data.bpi.USD.rate_float;
-            chart.data.labels.push(time.toLocaleTimeString());
-            chart.data.datasets[0].data.push(price);
-            
-            var predictedPrice = chart.data.datasets[1];
-            predictedPrice.data.push(price+5);
-            predictedPrice.backgroundColor = 'rgb(54, 162, 235)';
-            predictedPrice.borderColor = 'rgb(54, 162, 235)';
-            chart.update();
-            });
-        */
